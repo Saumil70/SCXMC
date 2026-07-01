651c0010d84fcc9eb2cbe76be4e492cf292338b3
Jay Sheth <jay@cactimedia.biz>
2026-07-01 19:16:49 +0530

    Revert "code changes for apollo client"
    This reverts commit 8851412e49650b35c713897fd9b11f6e12901f7a.

--- src/sxastarter/src/lib/apolloClient.ts
+++ src/sxastarter/src/lib/apolloClient.ts
@@ -1,33 +1,11 @@
 // lib/apolloClient.ts
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { createHttpLink } from '@apollo/client/link/http';
-
-const graphqlEndpoint =
-  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
-  (process.env.SITECORE_EDGE_URL
-    ? `${process.env.SITECORE_EDGE_URL}/v1/content/api/graphql/v1`
-    : 'https://edge-platform.sitecorecloud.io/v1/content/api/graphql/v1');
-
-const graphqlApiKey = process.env.NEXT_PUBLIC_GRAPHQL_API_KEY || process.env.SITECORE_API_KEY;
-
-const httpLink = createHttpLink({
-  uri: graphqlEndpoint,
-  fetchOptions: {
-    cache: 'no-store',
-  },
-});
-
-const authLink = setContext((_, { headers }) => ({
-  headers: {
-    ...headers,
-    ...(graphqlApiKey ? { Authorization: `Bearer ${graphqlApiKey}` } : {}),
-  },
-}));
 
 const client = new ApolloClient({
-  ssrMode: typeof window === 'undefined',
-  link: authLink.concat(httpLink),
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT, // XM Cloud GraphQL endpoint
+  headers: {
+    Authorization: `Bearer aWxGYTYyTmcxQ0ZGUCt4ekNxNUEwU25RV3JPSUo3QWdOcytOUi9sZ3VRdz18c291cmNldmVkMTU0MzQtanNpdGVjb3JleG1jNDEzLWRldjA0OTQtYzllNQ==`, // if API key needed
+  },
   cache: new InMemoryCache(),
 });
 
